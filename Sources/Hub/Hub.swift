3bcfb1a4f8a9bb41b5041ba28173909a581d265e
Damien <131157132+damienclr@users.noreply.github.com>
2025-03-27 14:22:38 +0100

    Update Hub.swift

--- Sources/Hub/Hub.swift
+++ Sources/Hub/Hub.swift
@@ -1,42 +1,33 @@
 import Foundation
 
-public struct Hub {
+public struct Hub {}
+
+public extension Hub {
     enum HubClientError: Error {
+        case parse
+        case authorizationRequired
         case unexpectedError
+        case httpStatusCode(Int)
     }
     
-    func loadConfig(modelFolder: URL) async throws -> Configuration {
-        let configPath = modelFolder.appendingPathComponent("config.json")
-        let tokenizerConfigPath = modelFolder.appendingPathComponent("tokenizer_config.json") 
-        let tokenizerPath = modelFolder.appendingPathComponent("tokenizer.json")
+    enum RepoType: String {
+        case models
+        case datasets
+        case spaces
+    }
+    
+    struct Repo {
+        public let id: String
+        public let type: RepoType
         
-        guard FileManager.default.fileExists(atPath: configPath.path),
-              FileManager.default.fileExists(atPath: tokenizerPath.path) else {
-            throw HubClientError.unexpectedError
+        public init(id: String, type: RepoType = .models) {
+            self.id = id
+            self.type = type
         }
-        
-        // Add this function in Hub
-        func configuration(fileURL: URL) throws -> Config {
-            let data = try Data(contentsOf: fileURL)
-            let parsed = try JSONSerialization.jsonObject(with: data)
-            guard let dictionary = parsed as? [NSString: Any] else {
-                throw HubClientError.unexpectedError
-            }
-            return Config(dictionary)
-        }
-        
-        let modelConfig = try configuration(fileURL: configPath)
-        let tokenizerData = try configuration(fileURL: tokenizerPath)
-        let tokenizerConfig = try? configuration(fileURL: tokenizerConfigPath)
-        
-        return Configuration(
-            modelConfig: modelConfig,
-            tokenizerConfig: tokenizerConfig,
-            tokenizerData: tokenizerData
-        )
     }
 }
 
+// MARK: - Configuration files with dynamic lookup
 @dynamicMemberLookup
 public struct Config {
     public private(set) var dictionary: [NSString: Any]
@@ -101,9 +92,117 @@ public struct Config {
     public var tokenValue: (UInt, String)? { value as? (UInt, String) }
 }
 
-public struct Configuration {
-    public let modelConfig: Config
-    public let tokenizerConfig: Config?
-    public let tokenizerData: Config
+public class LanguageModelConfigurationFromHub {
+    struct Configurations {
+        var modelConfig: Config
+        var tokenizerConfig: Config?
+        var tokenizerData: Config
+    }
+
+    private var configPromise: Task<Configurations, Error>? = nil
+
+    public init(
+        modelName: String,
+        hubApi: HubApi = .shared
+    ) {
+        self.configPromise = Task.init {
+            return try await self.loadConfig(modelName: modelName, hubApi: hubApi)
+        }
+    }
+    
+    public init(
+        modelFolder: URL,
+        hubApi: HubApi = .shared
+    ) {
+        self.configPromise = Task {
+            return try await self.loadConfig(modelFolder: modelFolder, hubApi: hubApi)
+        }
+    }
+
+    public var modelConfig: Config {
+        get async throws {
+            try await configPromise!.value.modelConfig
+        }
+    }
+
+    public var tokenizerConfig: Config? {
+        get async throws {
+            if let hubConfig = try await configPromise!.value.tokenizerConfig {
+                if let _ = hubConfig.tokenizerClass?.stringValue { return hubConfig }
+                guard let modelType = try await modelType else { return hubConfig }
+
+                if let fallbackConfig = Self.fallbackTokenizerConfig(for: modelType) {
+                    let configuration = fallbackConfig.dictionary.merging(hubConfig.dictionary, uniquingKeysWith: { current, _ in current })
+                    return Config(configuration)
+                }
+
+                var configuration = hubConfig.dictionary
+                configuration["tokenizer_class"] = "\(modelType.capitalized)Tokenizer"
+                return Config(configuration)
+            }
+
+            guard let modelType = try await modelType else { return nil }
+            return Self.fallbackTokenizerConfig(for: modelType)
+        }
+    }
+
+    public var tokenizerData: Config {
+        get async throws {
+            try await configPromise!.value.tokenizerData
+        }
+    }
+
+    public var modelType: String? {
+        get async throws {
+            try await modelConfig.modelType?.stringValue
+        }
+    }
+
+    func loadConfig(
+        modelName: String,
+        hubApi: HubApi = .shared
+    ) async throws -> Configurations {
+        let repo = Hub.Repo(id: modelName)
+        let modelFolder = hubApi.localRepoLocation(repo)
+        
+        return try await loadConfig(modelFolder: modelFolder, hubApi: hubApi)
+    }
+
+    func loadConfig(
+        modelFolder: URL,
+        hubApi: HubApi = .shared
+    ) async throws -> Configurations {
+        let modelConfig = try hubApi.configuration(fileURL: modelFolder.appending(path: "config.json"))
+        let tokenizerData = try hubApi.configuration(fileURL: modelFolder.appending(path: "tokenizer.json"))
+        var tokenizerConfig = try? hubApi.configuration(fileURL: modelFolder.appending(path: "tokenizer_config.json"))
+        
+        if let chatTemplateConfig = try? hubApi.configuration(fileURL: modelFolder.appending(path: "chat_template.json")),
+           let chatTemplate = chatTemplateConfig.chatTemplate?.stringValue {
+            if var configDict = tokenizerConfig?.dictionary {
+                configDict["chat_template"] = chatTemplate
+                tokenizerConfig = Config(configDict)
+            } else {
+                tokenizerConfig = Config(["chat_template": chatTemplate])
+            }
+        }
+        
+        return Configurations(
+            modelConfig: modelConfig,
+            tokenizerConfig: tokenizerConfig,
+            tokenizerData: tokenizerData
+        )
+    }
+
+    static func fallbackTokenizerConfig(for modelType: String) -> Config? {
+        guard let url = Bundle.module.url(forResource: "\(modelType)_tokenizer_config", withExtension: "json") else { return nil }
+        do {
+            let data = try Data(contentsOf: url)
+            let parsed = try JSONSerialization.jsonObject(with: data, options: [])
+            guard let dictionary = parsed as? [NSString: Any] else { return nil }
+            return Config(dictionary)
+        } catch {
+            return nil
+        }
+    }
 }
 
